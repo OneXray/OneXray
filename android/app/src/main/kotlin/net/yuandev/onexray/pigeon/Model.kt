1890c0ffcf08fd98ea1cb328d9788a86c17f24f5
yiguodev <147401898+yiguodev@users.noreply.github.com>
2026-07-01 21:45:07 +0800

    Dev 26.7.1 2 (#93)
    * Skip waiting for Apple build processing
    
    * Align native libXray invoke models
    
    * Update Apple bundle name and copyright
    
    * Add package manager update workflow
    
    * Log stopXray invoke failures

--- android/app/src/main/kotlin/net/yuandev/onexray/pigeon/Model.kt
+++ android/app/src/main/kotlin/net/yuandev/onexray/pigeon/Model.kt
@@ -44,3 +44,90 @@ data class StartVpnRequest(
     val metricsPort: String?,
     val coreInvokeText: String?,
 )
+
+@Serializable
+enum class LibXrayMethod {
+    @SerialName("getFreePorts")
+    GET_FREE_PORTS,
+
+    @SerialName("convertShareLinksToXrayJson")
+    CONVERT_SHARE_LINKS_TO_XRAY_JSON,
+
+    @SerialName("convertXrayJsonToShareLinks")
+    CONVERT_XRAY_JSON_TO_SHARE_LINKS,
+
+    @SerialName("countGeoData")
+    COUNT_GEO_DATA,
+
+    @SerialName("ping")
+    PING,
+
+    @SerialName("testXray")
+    TEST_XRAY,
+
+    @SerialName("runXray")
+    RUN_XRAY,
+
+    @SerialName("runXrayFromJson")
+    RUN_XRAY_FROM_JSON,
+
+    @SerialName("stopXray")
+    STOP_XRAY,
+
+    @SerialName("xrayVersion")
+    XRAY_VERSION,
+
+    @SerialName("getXrayState")
+    GET_XRAY_STATE,
+}
+
+@Serializable
+data class LibXrayEnvJson(
+    @SerialName("xray.location.config")
+    val configLocation: String? = null,
+    @SerialName("xray.location.confdir")
+    val confdirLocation: String? = null,
+    @SerialName("xray.location.asset")
+    val assetLocation: String? = null,
+    @SerialName("xray.location.cert")
+    val certLocation: String? = null,
+    @SerialName("xray.buf.readv")
+    val useReadV: String? = null,
+    @SerialName("xray.buf.splice")
+    val useFreedomSplice: String? = null,
+    @SerialName("xray.vmess.padding")
+    val useVmessPadding: String? = null,
+    @SerialName("xray.cone.disabled")
+    val useCone: String? = null,
+    @SerialName("xray.json.strict")
+    val useStrictJson: String? = null,
+    @SerialName("xray.ray.buffer.size")
+    val bufferSize: String? = null,
+    @SerialName("xray.browser.dialer")
+    val browserDialerAddress: String? = null,
+    @SerialName("xray.xudp.show")
+    val xudpLog: String? = null,
+    @SerialName("xray.xudp.basekey")
+    val xudpBaseKey: String? = null,
+    @SerialName("xray.tun.fd")
+    val tunFd: String? = null,
+)
+
+@Serializable
+data class RunXrayRequest(
+    val configPath: String? = null,
+)
+
+@Serializable
+data class LibXrayInvokeRequest(
+    val apiVersion: Int? = 1,
+    val method: LibXrayMethod? = null,
+    val env: LibXrayEnvJson? = null,
+    val payload: RunXrayRequest? = null,
+)
+
+@Serializable
+data class LibXrayInvokeResponse(
+    val success: Boolean? = null,
+    val error: String? = null,
+)
--- swift/All/Logger.swift
+++ swift/All/Logger.swift
@@ -2,25 +2,12 @@
 import Foundation
 import os
 
-@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
-let logger = Logger(subsystem: "net.yuandev.log", category: "debug")
+private let logger = Logger(subsystem: "net.yuandev.log", category: "debug")
 
-func YGLog(_ message: Any?) {
-    if #available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *) {
-        if let msg = message as? CustomDebugStringConvertible {
-            logger.error("\(String(reflecting: msg), privacy: .public)")
-        } else {
-            logger.error("YGLog can not log object")
-        }
-    } else {
-        if let msg = message as? CustomDebugStringConvertible {
-            print(msg)
-        } else {
-            print("YGLog can not log object")
-        }
-    }
+func YGLog(_ message: String) {
+    logger.error("\(message, privacy: .public)")
 }
 
 #else
-func YGLog(_ message: Any?) {}
+func YGLog(_ message: String) {}
 #endif
